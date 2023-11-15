ac113541b7af23c0cbcb08d41b180cf5de49a191
Eli Chase <eschase4@gmail.com>
2023-11-14 20:12:32 -0500

    added catch-all route to server.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,8 +4,6 @@ require('dotenv').config();
 const atlasPass = process.env.ATLAS_PASSWD;
 const atlasUser = process.env.ATLAS_USER
 
-console.log(atlasPass, atlasUser)
-
 mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://${atlasUser}:${atlasPass}@cluster0.ptwwxd7.mongodb.net/musician-match`);
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -18,6 +18,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
+
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
@@ -26,6 +28,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
